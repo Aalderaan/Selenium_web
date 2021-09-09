a7ed621166c50b2353568a160e13fc4821380bb1
Aalderaan <chyregd@gmail.com>
2021-09-10 01:07:15 +0300

    slider

--- src/test/java/hover/HoverTests.java
+++ src/test/java/hover/HoverTests.java
@@ -1,2 +1,20 @@
-package hover;public class HoverTests {
+package hover;
+
+import base.BaseTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.HoversPage;
+
+public class HoverTests extends BaseTests  {
+
+    @Test
+    public void testHoverUser1() {
+        var hoversPage = homePage.clickHovers();
+        var caption = hoversPage.hoverOverFigure(1);
+        Assert.assertTrue(caption.isCaptionDisplayed(), "Caption not dislplayed");
+        Assert.assertEquals(caption.getTitle(), "name: user1", "Caption title incorect");
+        Assert.assertEquals(caption.getLink(), "http://the-internet.herokuapp.com/users/1", "Caption link text incorrect");
+        Assert.assertTrue(caption.getLink().endsWith("/users/1"), "Link incorrect");
+    }
 }
