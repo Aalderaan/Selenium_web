08ec65a0c645b935568cf53a7fd9c4a641d0953a
Aalderaan <chyregd@gmail.com>
2021-10-10 11:41:14 +0300

    new adding

--- src/test/java/wait/WaitTests.java
+++ src/test/java/wait/WaitTests.java
@@ -1,2 +1,28 @@
-package wait;public class WaitTests {
+package wait;
+
+import base.BaseTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DynamicLoadingExamle1Page;
+import pages.DynamicLoadingExample2Page;
+
+public class WaitTests extends BaseTests {
+
+    @Test
+    public void testWaitUntilHidden() {
+        DynamicLoadingExamle1Page loadingPage = homePage.clickDynamicLoading()
+                .clickExample1();
+        loadingPage.clickStart();
+        Assert.assertEquals(loadingPage.getLoadedText(), "Hello World!",
+                "Loaded text incorrect");
+    }
+
+    @Test
+    public void testWaitUntilVisible() {
+       DynamicLoadingExample2Page loadingPage = homePage.clickDynamicLoading()
+                .clickExample2();
+        loadingPage.clickStart();
+        Assert.assertEquals(loadingPage.getLoadedText(), "Hello World!",
+                "Loaded text incorrect");
+    }
 }
